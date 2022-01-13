ab2dc2fe3d799bbb5004858a51b22a1c897cf318
stepan <sshishigin190101@gmail.com>
2022-01-13 17:38:10 +0300

    Добавлено распознавание

--- finadscript.py
+++ finadscript.py
@@ -1,15 +1,18 @@
-from fastapi import UploadFile
+from io import BytesIO
+
 
 try:
-    from PIL import Image
+    from PIL import Image, ImageEnhance
 except ImportError:
     import Image
 import pytesseract
 
 
 class FinAdWorker:
-
     @staticmethod
-    def work(file: UploadFile):
-        with file.read() as file:
-            return pytesseract.image_to_string(file, "rus")
+    def work(file):
+        img = BytesIO(file)
+        data = Image.open(img)
+        threshold = 40
+        data.point(lambda p: 255 if p > threshold else 0)
+        return pytesseract.image_to_string(data, "rus")
--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, File
+from starlette.middleware.cors import CORSMiddleware
 from finadscript import FinAdWorker
+
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST", "GET"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/fin-ad-analyze")
-async def root(file: UploadFile):
-    picture_text = FinAdWorker.work()
+async def upload_file(file: UploadFile = File(...)):
+    file_to_process = await file.read()
+    picture_text = FinAdWorker.work(file_to_process)
     print(picture_text)
     return {"picture_text": picture_text}
